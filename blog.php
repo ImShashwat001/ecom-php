137a55bab597c29bca68aaca5cfd87f394af34f9
Shashwat <shashwatpanda4@gmail.com>
2022-03-22 08:44:16 +0530

    initial commit

---
+++ blog.php
@@ -0,0 +1,18 @@
+<?php
+    ob_start();
+    // include header.php file
+    include ('header.php');
+?>
+
+<?php
+// include footer.php file
+include ('./Template/_blog.php');
+?>
+
+
+
+
+<?php
+// include footer.php file
+include ('footer.php');
+?>
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$keyId = 'rzp_test_o9HbizDtbNxe3h';
+$keySecret = '8AHn1lXzdVQaXJ5hTwCTB4sc';
+
+// Error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
